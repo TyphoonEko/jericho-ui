c54413e4f7c403b2a7d9f320ab190e77d6fee8f8
Jericho SIGUA <jericho.sigua@amadeus.com>
2025-11-01 22:12:33 +0800

    add docu

--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { MatDrawer } from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-top-nav',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './top-nav.component.html',
   styleUrl: './top-nav.component.scss'
 })
