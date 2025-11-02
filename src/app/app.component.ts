b4e303306649d782186e0220f4fd7709e702f61c
Eko <jericho.sigua6@gmail.com>
2025-11-02 10:45:26 +0800

    add pic slider; pics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,37 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { CommonModule } from '@angular/common';
 import { TopNavComponent } from "./top-nav/top-nav.component";
+import { MatIcon } from "@angular/material/icon";
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule, CommonModule, TopNavComponent],
+  imports: [
+    MatSidenavModule, 
+    MatButtonModule,
+    CommonModule,
+    TopNavComponent,
+    MatIcon,
+    SlickCarouselModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ac-service';
-  showFiller = false;
+
+  images = [
+    './assets/cool.png',
+    './assets/cool2.png',
+  ];
+
+  slideConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true
+  };
 }
